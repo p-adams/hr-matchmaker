295450380cf2f21b82ebd57c6daba9c830ed6b34
Paul Adams <Paul Adams>
2016-08-06 23:59:10 -0400

    update

--- pub/app.js
+++ pub/app.js
@@ -2,6 +2,27 @@ var socket = io();
 new Vue({
     el: "#app",
     data: {
-        msg: 'Meow'
+        msg: 'Meow',
+        username: '',
+        password: '',
+        logged: false,
+        user: ''
+    },
+    methods: {
+        login(){
+            var username = this.username
+            var password = this.password
+            this.logged = true
+            socket.emit('login', {usr: username, pass: password})
+            username=''
+            password=''
+        }
+    },
+    created(){
+        var self = this
+        socket.on('login', (login)=>{
+            self.user = login.usr
+            console.log(login.usr, login.pass)
+        })
     }
 })
--- server.js
+++ server.js
@@ -6,9 +6,22 @@ var express = require("express"),
     io = socketio(server);
 app.use(express.static("pub"));
 
+var employers = []
+
+var addEmployer = (name, password)=> {
+    var employer = {name: name, password: password}
+    employers.push(employer)
+    console.log(employers.length)
+}
+
 io.on('connection', (socket) => {
     console.log('Someone connected to server');
-    
+
+    socket.on('login', (login)=>{
+        addEmployer(login.usr, login.pass)
+        socket.emit('login', login)
+    })
+ 
     socket.on('disconnect', (socket) => {
        console.log('someone left the server'); 
     });
